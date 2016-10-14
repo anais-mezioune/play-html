bcd5e0df23f1d27b82c5fdc2219e2674302f68e6
Nicolas Giard <nicogiard@gmail.com>
2016-10-14 08:27:24 +0200

    initialisation du projet

---
+++ app/controllers/Produit.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import com.formation.exceptions.MetierException;
+import com.formation.services.ProduitService;
+import play.mvc.Controller;
+
+public class Produit extends Controller {
+
+    public static void create() {
+        render();
+    }
+
+    public static void save(String nom, Float prixUnitaire) {
+        com.formation.models.Produit produit = null;
+        try {
+            produit = ProduitService.creerProduit(nom, prixUnitaire);
+        } catch (MetierException e) {
+            e.printStackTrace();
+        }
+        render(produit);
+    }
+
+}
