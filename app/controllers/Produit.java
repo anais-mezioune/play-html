7bb09ee8851b7fcc3d724ef0e955001d7290e83e
Anais <anais.mezioune@gmail.com>
2016-10-24 17:03:46 +0200

    Page produit en cours

---
+++ app/controllers/Produit.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.data.validation.Required;
+import play.mvc.Controller;
+import play.mvc.With;
+
+
+public class Produit extends Controller {
+	
+	public static void produit() {
+        render();
+	}
+}
--- public/javascripts/outils.js
+++ public/javascripts/outils.js
@@ -1,5 +1,13 @@
 (function($){
 	
+	$("#main-nav-bar").click(function() {
+	    $("#main-nav-bar").prepend("<span class='bouton-moins'>-</span>");
+	    display(".bar-nav-bar");
+	    display("#main-nav");
+		
+	});
+	
+	
 	$(".section-list > li:first").click(function() {
 	    display(".section-list > li:first .content-description");
 	});
@@ -20,7 +28,11 @@
 
 
 
-/*function display($(content))){
+/*
+ * .index() -> retourne l'index de l'element courant
+ * 
+ * 
+ function display($(content))){
 	if ($(content).css('display', 'none')){
 		$content.css('display', 'block');
     } else{
